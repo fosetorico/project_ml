90a7a044f9dc817908c4689357cef5ce82defd2d
unknown <vallee.neutral@gmail.com>
2024-11-14 15:37:35 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+# setup.py is responsible for creating machine learning application as a pakage, as well as deploying pypi.
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='project_ml',
+version='0.0.1',
+author='valentine',
+author_email='vallee.neutral@gmail.com',
+packages=find_packages(),
+# install_requires=['pandas','numpy','seaborn']
+install_requires=get_requirements('requirements.txt')
+)
